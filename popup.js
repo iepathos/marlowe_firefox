63f360a2c5c23d075ecbc3672b7228fe09132174
Glen Baker <iepathos@gmail.com>
2017-04-27 00:17:02 -0400

    Updated popup.js with new marlowe usent api change

--- popup.js
+++ popup.js
@@ -1,15 +1,27 @@
 $("#checkPage").click(function(){
-  $.ajax({
-    type: 'POST', 
-    url: 'https://morning-harbor-96808.herokuapp.com', 
-    dataType: 'json',
-    data: $('#marlowe').serialize(),
-    success: function (data) {
-      $("#resp").html("<h3>" + Math.round(data.objectivity * 100) + "% objective</h3>");
-    },
-    beforeSend:function(){
-      $("#resp").html("Loading...")
-    }
+  var scores = [];
+  var urls = ['https://morning-harbor-96808.herokuapp.com', 'https://desolate-bastion-86556.herokuapp.com']
+  $(urls).each(function() {
+    var url = this;
+    $.ajax({
+      type: 'POST', 
+      url: url, 
+      dataType: 'json',
+      data: $('#marlowe').serialize(),
+      success: function (data) {
+        scores.push(Math.round(data.objectivity * 100));
+        if (scores.length == urls.length) {
+          sum = 0;
+          for(var i = 0, len = scores.length; i < len; i++) {
+              sum += scores[i];
+          }
+          $("#resp").html("<h3>" + sum/2 + "% objective</h3>");
+        }
+      },
+      beforeSend:function(){
+        $("#resp").html("Loading...")
+      }
+    });
   });
   return false;
 });
