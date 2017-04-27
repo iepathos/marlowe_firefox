b8498c4e85cd0701f6a51e75d5115eb81491c07f
Glen Baker <iepathos@gmail.com>
2017-04-27 00:11:15 -0400

    Updated to use marlowe usent objectivity api as well and take an average from the two, version 0.4

--- background.js
+++ background.js
@@ -1,13 +1,25 @@
 function getObjectivity(info, tab) {
-  $.ajax({
-    type: 'POST', 
-    url: 'https://morning-harbor-96808.herokuapp.com', 
-    dataType: 'json',
-    data: {"text": info.selectionText},
-    success: function (data) {
-      alert(Math.round(data.objectivity * 100) + "% objective");
-    }
-  });    
+  var scores = [];
+  var urls = ['https://morning-harbor-96808.herokuapp.com', 'https://desolate-bastion-86556.herokuapp.com']
+  $(urls).each(function() {
+    var url = this;
+    $.ajax({
+      type: 'POST', 
+      url: url, 
+      dataType: 'json',
+      data: {"text": info.selectionText},
+      success: function (data) {
+        scores.push(Math.round(data.objectivity * 100));
+        if (scores.length == urls.length) {
+          sum = 0;
+          for(var i = 0, len = scores.length; i < len; i++) {
+              sum += scores[i];
+          }
+          alert(sum/2 + "% objective");
+        }
+      }
+    });
+  });
 }
 chrome.contextMenus.create({
   title: "Objectivity: %s", 
